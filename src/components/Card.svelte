39585b672d477ace1c943df47ff09ca9409b61f6
Kfiatek430 <110732078+Kfiatek430@users.noreply.github.com>
2025-01-06 14:31:57 +0100

    Poprawa logiki Card

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
+    export let profilePicturePath: string = '';
+    export let name: string = 'Unknown';
+    export let description: string = 'No description available.';
+    export let link: string = '#'; 
     import defaultProfilePicture from '../assets/images/defaultProfilePicture.png';
 </script>
 
 <div class="max-w-sm  border  rounded-lg shadow bg-gray-800 border-gray-700 w-4/5 sm:w-64 sm:h-160">
     <div class="p-5 flex justify-center">
-        <img class="rounded-t-lg w-64" src={defaultProfilePicture} alt="" />
+        <img class="rounded-t-lg w-64" src={profilePicturePath || defaultProfilePicture} alt="" />
     </div>
     <div class="p-5">
-        <a href="#">
-            <h5 class="mb-2 text-2xl text-white font-bold tracking-tighttext-white">Noteworthy technology acquisitions 2021</h5>
+        <a href="{link}">
+            <h5 class="mb-2 text-2xl text-white font-bold tracking-tighttext-white">{name}</h5>
         </a>
-        <p class="mb-3 font-normal text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg focus:ring-4 focus:outline-none bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
+        <p class="mb-3 font-normal text-gray-400">{description}</p>
+        <a href="{link}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg focus:ring-4 focus:outline-none bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
             Read more
              <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -8,7 +8,7 @@
                 </svg>
             </div>
             <input type="search" id="default-search" class="block w-full p-2 sm:p-4 sm:pl-9 ps-10 text-sm rounded-lg bg-gray-700 border-2 border-gray-700 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="Search criminals..." required />
-            <button type="submit" class="text-white absolute end-2.5 bottom-1.5 sm:bottom-2.5 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-2 sm:px-4 py-1 sm:py-2 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">Szukaj</button>
+            <button type="submit" class="text-white absolute end-2.5 bottom-1.5 sm:bottom-2.5 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-2 sm:px-4 py-1 sm:py-2 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">Search</button>
         </div>
     </form>
 </header>
