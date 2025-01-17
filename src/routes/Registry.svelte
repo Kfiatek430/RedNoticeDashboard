575c965806d4c868ea0ba3fbf0875602f383ac03
Kfiatek430 <kfiatek43@gmail.com>
2025-01-17 11:56:52 +0100

    Rozpoczęcie implementacji fetchowania danych

--- src/routes/Registry.svelte
+++ src/routes/Registry.svelte
@@ -1,5 +1,44 @@
 <script lang="ts">
+    import { onMount } from "svelte";
     import Card from "../components/Card.svelte";
+
+    const noticesLink = "https://ws-public.interpol.int/notices/v1/red?&resultPerPage=50&page=1";
+    let criminals = [];
+
+    class Criminal {
+        public name: string;
+        public forename: string;
+        public description: string;
+        public dateOfBirth: string;
+        public profilePicturePath: string;
+
+        constructor(href: string) {
+            this.fetchCriminalData(href);
+            this.name = "Unknown";
+            this.forename = "Unknown";
+            this.description = "No description available.";
+            this.dateOfBirth = "Unknown";
+            this.profilePicturePath = href;
+        }
+
+        async fetchCriminalData(href: string) {
+
+        }
+    }
+
+    async function fetchNotices() {
+        const response = await fetch(noticesLink);
+        let data = await response.json();
+        data = data._embedded.notices;
+
+        data.forEach((element: Object) => {
+            criminals.push(new Criminal(element._links.self.href));
+        });
+    }
+
+    onMount(async () => {
+        await fetchNotices();
+    });
 </script>
 
 <div class="flex flex-col items-center justify-center gap-3 p-4 text-white">
@@ -8,7 +47,4 @@
 </div>
     
 <div class="flex justify-center gap-6 flex-wrap p-4">
-    {#each new Array(16) as _}
-        <Card />
-    {/each}
 </div>
