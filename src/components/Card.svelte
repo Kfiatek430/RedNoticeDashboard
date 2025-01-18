2ead298a57a31b288de8c2d3160093aea4e6079e
Kfiatek430 <110732078+Kfiatek430@users.noreply.github.com>
2025-01-18 01:48:13 +0100

    Implementacja fetchowania i wyświetlania danych

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-    export let profilePicturePath: string = '';
-    export let name: string = 'Unknown';
-    export let description: string = 'No description available.';
-    export let link: string = '#'; 
+    export let profilePicturePath: string;
+    export let name: string;
+    export let description: string;
+    export let link: string = '#';
+
     import defaultProfilePicture from '../assets/images/defaultProfilePicture.png';
 </script>
 
@@ -17,7 +18,7 @@
         <p class="mb-3 font-normal text-gray-400">{description}</p>
         <a href="{link}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg focus:ring-4 focus:outline-none bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
             Read more
-             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
         </a>
--- src/routes/Registry.svelte
+++ src/routes/Registry.svelte
@@ -3,41 +3,163 @@
     import Card from "../components/Card.svelte";
 
     const noticesLink = "https://ws-public.interpol.int/notices/v1/red?&resultPerPage=50&page=1";
-    let criminals = [];
+    let criminals: Criminal[] = [];
+
+    interface BasicNotice {
+        date_of_birth: string;
+        entity_id: string;
+        forename: string;
+        name: string;
+        nationalities: string[];
+        _links: {
+            images: {
+                href: string;
+            };
+            self: {
+                href: string;
+            };
+            thumbnail: {
+                href: string;
+            };
+        };
+    }
+
+    interface ExtendedNotice {
+        date_of_birth: string;
+        distinguishing_marks: string | null;
+        weight: number | null;
+        nationalities: string[];
+        entity_id: string;
+        eyes_colors_id: string | null;
+        sex_id: string;
+        place_of_birth: string;
+        forename: string;
+        arrest_warrants: {
+            charge: string;
+            issuing_country_id: string;
+            charge_translation: string | null;
+        }[];
+        country_of_birth_id: string;
+        hairs_id: string | null;
+        name: string;
+        languages_spoken_ids: string[];
+        height: number | null;
+        _embedded: {
+            links: {
+                forename: string;
+                color: string;
+                name: string;
+                entity_id: string;
+                _links: {
+                    self: {
+                        href: string;
+                    };
+                    thumbnail: {
+                        href: string;
+                    };
+                };
+            }[];
+        };
+        _links: {
+            self: {
+                href: string;
+            };
+            images: {
+                href: string;
+            };
+            thumbnail: {
+                href: string;
+            };
+        };
+    }
 
     class Criminal {
         public name: string;
         public forename: string;
-        public description: string;
         public dateOfBirth: string;
         public profilePicturePath: string;
+        public externalLink: string;
+        public distinguishingMarks: string | null;
+        public weight: number | null;
+        public eyesColorsId: string | null;
+        public sexId: string;
+        public placeOfBirth: string;
+        public arrestWarrants: {
+            charge: string;
+            issuingCountryId: string;
+            chargeTranslation: string | null;
+        }[];
+        public countryOfBirthId: string;
+        public hairsId: string | null;
+        public languagesSpokenIds: string[];
+        public height: number | null;
 
         constructor(href: string) {
-            this.fetchCriminalData(href);
+            this.externalLink = href;
+
             this.name = "Unknown";
             this.forename = "Unknown";
-            this.description = "No description available.";
             this.dateOfBirth = "Unknown";
-            this.profilePicturePath = href;
+            this.profilePicturePath = "";
+            this.distinguishingMarks = null;
+            this.weight = null;
+            this.eyesColorsId = null;
+            this.sexId = "Unknown";
+            this.placeOfBirth = "Unknown";
+            this.arrestWarrants = [];
+            this.countryOfBirthId = "Unknown";
+            this.hairsId = null;
+            this.languagesSpokenIds = [];
+            this.height = null;
+        }
+
+        async run() {
+            await this.fetchCriminalData(this.externalLink);
         }
 
         async fetchCriminalData(href: string) {
+            const response = await fetch(href);
+            let data: ExtendedNotice = await response.json();
 
+            this.name = data.name;
+            this.forename = data.forename;
+            this.dateOfBirth = data.date_of_birth;
+            this.profilePicturePath = data._links.thumbnail.href;
+            this.distinguishingMarks = data.distinguishing_marks;
+            this.weight = data.weight;
+            this.eyesColorsId = data.eyes_colors_id;
+            this.sexId = data.sex_id;
+            this.placeOfBirth = data.place_of_birth;
+            this.arrestWarrants = data.arrest_warrants.map(warrant => ({
+                charge: warrant.charge,
+                issuingCountryId: warrant.issuing_country_id,
+                chargeTranslation: warrant.charge_translation
+            }))
+            this.countryOfBirthId = data.country_of_birth_id;
+            this.hairsId = data.hairs_id;
+            this.languagesSpokenIds = data.languages_spoken_ids;
+            this.height = data.height;
         }
     }
 
     async function fetchNotices() {
         const response = await fetch(noticesLink);
-        let data = await response.json();
-        data = data._embedded.notices;
+        const data = await response.json();
 
-        data.forEach((element: Object) => {
-            criminals.push(new Criminal(element._links.self.href));
+        const notices: BasicNotice[] = data._embedded.notices;
+
+        const criminalPromises = notices.map(async (element) => {
+            let criminal = new Criminal(element._links.self.href);
+            await criminal.run();
+            return criminal;
         });
+
+        criminals = await Promise.all(criminalPromises);
     }
 
     onMount(async () => {
         await fetchNotices();
+        console.log(criminals);
     });
 </script>
 
@@ -45,6 +167,13 @@
     <h1 class="text-3xl sm:text-4xl md:text-5xl text-center text-white">Criminals Registry</h1>
     <p class="text-base md:text-lg flex justify-center text-gray text-center">Browse and manage detailed records of offenders with advanced sorting and filtering options.</p>
 </div>
-    
 <div class="flex justify-center gap-6 flex-wrap p-4">
+    {#each criminals as criminal}
+        <Card
+            name={criminal.name + " " + criminal.forename}
+            description={criminal.arrestWarrants[0].charge || "No charge available"}
+            profilePicturePath={criminal.profilePicturePath}
+            link={criminal.externalLink}
+        />
+    {/each}
 </div>
