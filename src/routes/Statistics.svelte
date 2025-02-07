6b2301b6e88c472a64623bc29eab130e8b7a1808
Wojciech Rachwal <wojci3ch.rachwal@gmail.com>
2025-02-07 11:36:40 +0100

    chart example

--- src/routes/Statistics.svelte
+++ src/routes/Statistics.svelte
@@ -1,4 +1,60 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import Chart from "chart.js/auto";
+
+    let portfolio;
+    const data = {
+        labels: ["Expenses", "Savings", "Investments"],
+        datasets: [
+            {
+                label: "My First Dataset",
+                data: [300, 50, 100],
+                backgroundColor: ["#7000e1", "#fc8800", "#00b0e8"],
+                // hoverOffset: 4,
+                borderWidth: 0,
+            },
+        ],
+    };
+    const config = {
+        type: "bar",
+        data: data,
+        options: {
+            responsive: true,
+            cutout: "95%",
+            spacing: 2,
+            plugins: {
+                legend: {
+                    position: "bottom",
+                    display: true,
+                    labels: {
+                        usePointStyle: true,
+                        padding: 20,
+                        font: {
+                            size: 14,
+                        },
+                    },
+                },
+                title: {
+                    display: true,
+                    text: "My Personal Portfolio",
+                },
+            },
+        },
+    };
+    onMount(() => {
+        const ctx = portfolio!.getContext("2d");
+        var myChart = new Chart(ctx, config);
+    });
+</script>
+
 <div class="flex flex-col items-center justify-center gap-3 p-4 text-white">
-    <h1 class="text-3xl sm:text-4xl md:text-5xl text-center text-white">Statistics</h1>
-    <p class="text-base md:text-lg flex justify-center text-gray text-center">Explore comprehensive crime statistics with tools for filtering and data analysis.</p>
+    <h1 class="text-3xl sm:text-4xl md:text-5xl text-center text-white">
+        Statistics
+    </h1>
+    <p class="text-base md:text-lg flex justify-center text-gray text-center">
+        Explore comprehensive crime statistics with tools for filtering and data
+        analysis.
+    </p>
+
+    <canvas bind:this={portfolio} width={400} height={400} />
 </div>
--- src/routes/[id].svelte
+++ src/routes/[id].svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import defaultProfilePicture from '../assets/images/defaultProfilePicture.png';
+    import defaultProfilePicture from "../assets/images/defaultProfilePicture.png";
     import type { ExtendedNotice } from "../types/ExtendedNotice";
 
-    export let params: { id: string } = { id: '' };
+    export let params: { id: string } = { id: "" };
     $: id = params.id;
 
     class Criminal {
@@ -65,28 +65,52 @@
             const response = await fetch(href);
             let data: ExtendedNotice = await response.json();
 
-            if(this.name) {
-                this.name = data.name.split(' ').map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+            if (this.name) {
+                this.name = data.name
+                    .split(" ")
+                    .map(
+                        (word) =>
+                            word.charAt(0).toUpperCase() +
+                            word.slice(1).toLowerCase(),
+                    )
+                    .join(" ");
             }
 
-            if(this.forename) {
-                this.forename = data.forename.split(' ').map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+            if (this.forename) {
+                this.forename = data.forename
+                    .split(" ")
+                    .map(
+                        (word) =>
+                            word.charAt(0).toUpperCase() +
+                            word.slice(1).toLowerCase(),
+                    )
+                    .join(" ");
             }
 
-            this.dateOfBirth = new Date(data.date_of_birth).toLocaleDateString('en-GB', { year: 'numeric', month: 'long', day: 'numeric' });
+            this.dateOfBirth = new Date(data.date_of_birth).toLocaleDateString(
+                "en-GB",
+                { year: "numeric", month: "long", day: "numeric" },
+            );
             this.nationalities = data.nationalities;
             this.profilePicturePath = data._links.thumbnail.href;
 
-            if(this.placeOfBirth) {
-                this.placeOfBirth = data.place_of_birth!.split(' ').map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+            if (this.placeOfBirth) {
+                this.placeOfBirth = data
+                    .place_of_birth!.split(" ")
+                    .map(
+                        (word) =>
+                            word.charAt(0).toUpperCase() +
+                            word.slice(1).toLowerCase(),
+                    )
+                    .join(" ");
             }
 
             this.countryOfBirthId = data.country_of_birth_id;
 
-            if(data.sex_id === 'M') {
-                this.sex = 'Male';
+            if (data.sex_id === "M") {
+                this.sex = "Male";
             } else {
-                this.sex = 'Female';
+                this.sex = "Female";
             }
 
             this.weight = data.weight;
@@ -98,10 +122,18 @@
 
             this.languagesSpokenIds = data.languages_spoken_ids;
 
-            this.arrestWarrants = data.arrest_warrants.map(warrant => ({
-                charge: warrant.charge.split(' ').map((word, index) => index === 0 ? word.charAt(0).toUpperCase() + word.slice(1).toLowerCase() : word.toLowerCase()).join(' '),
+            this.arrestWarrants = data.arrest_warrants.map((warrant) => ({
+                charge: warrant.charge
+                    .split(" ")
+                    .map((word, index) =>
+                        index === 0
+                            ? word.charAt(0).toUpperCase() +
+                              word.slice(1).toLowerCase()
+                            : word.toLowerCase(),
+                    )
+                    .join(" "),
                 issuingCountryId: warrant.issuing_country_id,
-                chargeTranslation: warrant.charge_translation
+                chargeTranslation: warrant.charge_translation,
             }));
         }
     }
@@ -110,7 +142,9 @@
     let dataFetched = false;
 
     onMount(async () => {
-        criminal = new Criminal(`https://ws-public.interpol.int/notices/v1/red/${id}`);
+        criminal = new Criminal(
+            `https://ws-public.interpol.int/notices/v1/red/${id}`,
+        );
         await criminal.run();
         dataFetched = true;
 
@@ -121,9 +155,16 @@
 
     onMount(async () => {
         try {
-            const res = await fetch("https://api.first.org/data/v1/countries?limit=260");
+            const res = await fetch(
+                "https://api.first.org/data/v1/countries?limit=260",
+            );
             const data = await res.json();
-            countryMap = Object.fromEntries(Object.entries(data.data).map(([code, info]) => [code, info.country]));
+            countryMap = Object.fromEntries(
+                Object.entries(data.data).map(([code, info]) => [
+                    code,
+                    info.country,
+                ]),
+            );
         } catch (error) {
             console.error("Error fetching country data:", error);
         }
@@ -132,62 +173,113 @@
 
 <div class="w-full">
     {#if dataFetched}
-    <h1 class="text-3xl sm:text-4xl md:text-5xl text-center text-white mb-5">{criminal.name + " " + criminal.forename}</h1>
-        <div class="flex flex-col md:flex-row items-start justify-center gap-5 p-4 text-white w-full">
+        <h1
+            class="text-3xl sm:text-4xl md:text-5xl text-center text-white mb-5"
+        >
+            {criminal.name + " " + criminal.forename}
+        </h1>
+        <div
+            class="flex flex-col md:flex-row items-start justify-center gap-5 p-4 text-white w-full"
+        >
             <aside class="w-full md:w-1/3 flex flex-col gap-2 p-4 font-bold">
-                <img class="rounded-lg w-full md:w-64" src={criminal.profilePicturePath || defaultProfilePicture} alt="" draggable="false" />
+                <img
+                    class="rounded-lg w-full md:w-64"
+                    src={criminal.profilePicturePath || defaultProfilePicture}
+                    alt=""
+                    draggable="false"
+                />
                 <div class="flex items-center gap-3">
                     <p>Nationalities:</p>
                     {#each criminal.nationalities as nationality}
                         <div class="relative group">
-                            <img src={`https://www.flagsapi.com/${nationality}/flat/24.png`} alt="" draggable="false" class="cursor-pointer" />
-                            <span class="absolute bottom-5 left-1/2 transform -translate-x-1/2 hidden group-hover:block bg-black text-white text-xs rounded px-2 py-1 whitespace-nowrap min-w-max">
+                            <img
+                                src={`https://www.flagsapi.com/${nationality}/flat/24.png`}
+                                alt=""
+                                draggable="false"
+                                class="cursor-pointer"
+                            />
+                            <span
+                                class="absolute bottom-5 left-1/2 transform -translate-x-1/2 hidden group-hover:block bg-black text-white text-xs rounded px-2 py-1 whitespace-nowrap min-w-max"
+                            >
                                 {countryMap[nationality] || nationality}
                             </span>
                         </div>
                     {/each}
                 </div>
-            
+
                 {#if criminal.countryOfBirthId}
-                <div class="flex items-center gap-3">
-                    <p>Country of birth:</p>
-                    
-                    <div class="relative group">
-                        <img src={`https://www.flagsapi.com/${criminal.countryOfBirthId}/flat/24.png`} alt="" draggable="false" />
-                        <span class="absolute bottom-5 left-1/2 transform -translate-x-1/2 hidden group-hover:block bg-black text-white text-xs rounded px-2 py-1 whitespace-nowrap min-w-max">
-                            {countryMap[criminal.countryOfBirthId] || criminal.countryOfBirthId}
-                        </span>
+                    <div class="flex items-center gap-3">
+                        <p>Country of birth:</p>
+
+                        <div class="relative group">
+                            <img
+                                src={`https://www.flagsapi.com/${criminal.countryOfBirthId}/flat/24.png`}
+                                alt=""
+                                draggable="false"
+                            />
+                            <span
+                                class="absolute bottom-5 left-1/2 transform -translate-x-1/2 hidden group-hover:block bg-black text-white text-xs rounded px-2 py-1 whitespace-nowrap min-w-max"
+                            >
+                                {countryMap[criminal.countryOfBirthId] ||
+                                    criminal.countryOfBirthId}
+                            </span>
+                        </div>
                     </div>
-                </div>
                 {/if}
 
-                <p>Date of birth: <span class="font-normal">{criminal.dateOfBirth}</span></p>
+                <p>
+                    Date of birth: <span class="font-normal"
+                        >{criminal.dateOfBirth}</span
+                    >
+                </p>
 
                 {#if criminal.placeOfBirth}
-                    <p>Place of birth: <span class="font-normal">{criminal.placeOfBirth}</span></p>
+                    <p>
+                        Place of birth: <span class="font-normal"
+                            >{criminal.placeOfBirth}</span
+                        >
+                    </p>
                 {/if}
-                
+
                 {#if criminal.sex}
                     <p>Sex: <span class="font-normal">{criminal.sex}</span></p>
                 {/if}
-                
+
                 {#if criminal.height}
-                    <p>Height: <span class="font-normal">{criminal.height} cm</span></p>
+                    <p>
+                        Height: <span class="font-normal"
+                            >{criminal.height} cm</span
+                        >
+                    </p>
                 {/if}
-                
+
                 {#if criminal.weight}
-                    <p>Weight: <span class="font-normal">{criminal.weight} kg</span></p>
+                    <p>
+                        Weight: <span class="font-normal"
+                            >{criminal.weight} kg</span
+                        >
+                    </p>
                 {/if}
 
                 {#if criminal.eyesColorsId}
-                    <p>Eyes: <span class="font-normal">{criminal.eyesColorsId}</span></p>
+                    <p>
+                        Eyes: <span class="font-normal"
+                            >{criminal.eyesColorsId}</span
+                        >
+                    </p>
                 {/if}
-                
+
                 {#if criminal.hairsId}
-                    <p>Hairs: <span class="font-normal">{criminal.hairsId}</span></p>
+                    <p>
+                        Hairs: <span class="font-normal"
+                            >{criminal.hairsId}</span
+                        >
+                    </p>
                 {/if}
             </aside>
-            <div class="w-full md:w-2/3 h-full flex flex-col gap-2 justify-start p-4">
+            <div
+                class="w-full md:w-2/3 h-full flex flex-col gap-2 justify-start p-4"
+            >
                 <h2 class="font-bold text-xl">Arrest warrants:</h2>
                 <ul>
                     {#each criminal.arrestWarrants as warrant}
@@ -198,7 +290,9 @@
                 </ul>
 
                 {#if criminal.distinguishingMarks}
-                    <h2 class="font-bold text-xl mt-3">Distinguishing marks:</h2>
+                    <h2 class="font-bold text-xl mt-3">
+                        Distinguishing marks:
+                    </h2>
                     <p>{criminal.distinguishingMarks}</p>
                 {/if}
             </div>
