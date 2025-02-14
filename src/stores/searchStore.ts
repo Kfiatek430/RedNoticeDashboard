2e91c140c98e4b21084aa6c3b73b54ae2f46b7c1
Kfiatek430 <110732078+Kfiatek430@users.noreply.github.com>
2025-02-14 10:47:51 +0100

    Fix Not Found info

--- src/stores/searchStore.ts
+++ src/stores/searchStore.ts
@@ -1,3 +1,4 @@
 import { writable } from "svelte/store";
+import type { BasicNotice } from "../types/BasicNotice";
 
-export const searchQuery = writable([]);
+export const searchQuery = writable<BasicNotice[]>([]);
