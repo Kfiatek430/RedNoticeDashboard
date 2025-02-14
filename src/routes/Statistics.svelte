1fbaed88496fb936e7756e97fe1b0e6c7706c8f8
radek1248 <rbrunowski@wp.pl>
2025-02-14 11:38:21 +0100

    fix some issues

--- src/routes/Statistics.svelte
+++ src/routes/Statistics.svelte
@@ -170,13 +170,12 @@
   });
 </script>
 
-<div class="flex flex-col items-center justify-center gap-3 p-4 text-white">
+<div class="flex flex-col items-center justify-center gap-3 p-4 text-white w-3/5">
   <h1 class="text-3xl sm:text-4xl md:text-5xl text-center text-white">
     Statistics
   </h1>
   <p class="text-base md:text-lg flex justify-center text-gray text-center">
-    Explore comprehensive crime statistics with tools for filtering and data
-    analysis.
+    Explore comprehensive crime statistics.
   </p>
 
   <canvas
